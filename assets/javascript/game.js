8b6e20567999263e44eaf26b7be0f7e1e4489a0c
bradham <bradham@users.noreply.github.com>
2019-08-13 20:46:31 -0500

    Refactored counter to be totalScore. Added logic for total wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,10 @@ var targetNumber = 53;
 //Show the user the number that was selected
 $("#number-to-guess").text(targetNumber);
 
-var counter = 0;
+var totalScore = 0;
 var numberOptions = [10, 5, 3, 7];
+var wins = 0;
+var losses = 0;
 
 //Create an image for each item in the array numberOptions 
 for (var i = 0; i < numberOptions.length; i++) {
@@ -14,7 +16,7 @@ for (var i = 0; i < numberOptions.length; i++) {
     //Assign each <img> a value from the numberOptions array.
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     console.log("imageCrystal " + i + " has value " + numberOptions[i]);
-    
+
     $("#crystals").append(imageCrystal);
 }
 
@@ -37,15 +39,21 @@ $(".crystal-image").on("click", function () {
     crystalValue = parseInt(crystalValue);
     console.log("crystalValue is: " + crystalValue);
 
-    //Add the crystal's value to the total
-    counter += crystalValue;
-    console.log("counter variable is " + counter);
+    //Add the crystal's value to the total and display the total
+    totalScore += crystalValue;
+    console.log("totalScore variable is " + totalScore);
+    $("#total-score").text(totalScore);
 
-    if (counter === targetNumber) {
+
+    if (totalScore === targetNumber) {
         console.log("You Win!");
+        $("#wins-losses-message").text("You win!");
+        wins++;
+        $("#wins-total").append(wins);
+
     }
 
-    else if (counter >= targetNumber) {
+    else if (totalScore >= targetNumber) {
         console.log("You Loose!");
     }
 });
